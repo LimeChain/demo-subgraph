9b2ad83b9afe86cfbe8d43dbda74691cefc36d9b
Victor Velev <vivelev@icloud.com>
2021-11-22 23:52:23 -0500

    bump and refactor (rm cryptopunks + move util test funcs)

--- tests/gravity/utils.ts
+++ tests/gravity/utils.ts
@@ -1,7 +1,39 @@
-import { NewGravatar } from "../../generated/Gravity/Gravity"
-
-import { newMockEvent } from "matchstick-as/assembly/index"
 import { Address, ethereum } from "@graphprotocol/graph-ts"
+import { newMockEvent } from "matchstick-as/assembly/index"
+
+import { Gravatar } from "../../generated/schema"
+import { NewGravatar, Gravity } from "../../generated/Gravity/Gravity"
+import { handleNewGravatar } from "../../src/gravity"
+
+export function handleNewGravatars(events: NewGravatar[]): void {
+  events.forEach(event => {
+      handleNewGravatar(event)
+  })
+}
+
+let contractAddress = Address.fromString("0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7")
+let contract = Gravity.bind(contractAddress)
+
+export function saveGravatarFromContract(gravatarId: string): void {
+  let contractGravatar = contract.getGravatar(contractAddress)
+
+  let gravatar = new Gravatar(gravatarId)
+  gravatar.setString("value0", contractGravatar.value0)
+  gravatar.setString("value1", contractGravatar.value1)
+  gravatar.save()
+}
+
+export function trySaveGravatarFromContract(gravatarId: string): void {
+  let contractGravatar = contract.try_getGravatar(contractAddress)
+
+  if (!contractGravatar.reverted) {
+      let contractGravatarValue = contractGravatar.value
+      let gravatar = new Gravatar(gravatarId)
+      gravatar.setString("value0", contractGravatarValue.value0)
+      gravatar.setString("value1", contractGravatarValue.value1)
+      gravatar.save()
+  }
+}
 
 export function createNewGravatarEvent(id: i32, ownerAddress: string, displayName: string, imageUrl: string): NewGravatar {
     let mockEvent = newMockEvent()
