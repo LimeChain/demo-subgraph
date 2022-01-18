7dbc1a3c8392beea26516796edc39e69a326afac
georg-getz <georgi.getz@outlook.com>
2022-01-18 09:46:46 +0200

    put exclamation marks to avoid compiler complaining about null union type

--- src/token-lock-wallet.ts
+++ src/token-lock-wallet.ts
@@ -11,42 +11,38 @@ import {
 import { TokenLockWallet } from '../generated/schema'
 
 export function handleTokensReleased(event: TokensReleased): void {
-  let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())
-  tokenLockWallet!.tokensReleased = tokenLockWallet!.tokensReleased.plus(event.params.amount)
-  tokenLockWallet!.save()
+  let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())!
+  tokenLockWallet.tokensReleased = tokenLockWallet.tokensReleased.plus(event.params.amount)
+  tokenLockWallet.save()
 }
 
 export function handleTokensWithdrawn(event: TokensWithdrawn): void {
-  let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())
+  let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())!
   tokenLockWallet.tokensWithdrawn = tokenLockWallet.tokensWithdrawn.plus(event.params.amount)
   tokenLockWallet.save()
 }
 
 export function handleTokensRevoked(event: TokensRevoked): void {
-  let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())
+  let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())!
   tokenLockWallet.tokensRevoked = tokenLockWallet.tokensRevoked.plus(event.params.amount)
   tokenLockWallet.save()
 }
 
 export function handleManagerUpdated(event: ManagerUpdated): void {
   let id = event.address.toHexString()
-  log.warning('address id: {}', [id])
-  let tokenLockWallet = TokenLockWallet.load(id)
-  if (tokenLockWallet == null) {
-    log.warning("wallet doesn't exist", [])
-  }
+  let tokenLockWallet = TokenLockWallet.load(id)!
   tokenLockWallet.manager = event.params._newManager
   tokenLockWallet.save()
 }
 
 export function handleApproveTokenDestinations(event: ApproveTokenDestinations): void {
-  let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())
+  let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())!
   tokenLockWallet.tokenDestinationsApproved = true
   tokenLockWallet.save()
 }
 
 export function handleRevokeTokenDestinations(event: RevokeTokenDestinations): void {
-  let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())
+  let tokenLockWallet = TokenLockWallet.load(event.address.toHexString())!
   tokenLockWallet.tokenDestinationsApproved = false
   tokenLockWallet.save()
 }
