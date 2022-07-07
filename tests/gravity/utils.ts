419725e44d9d0b8682fc13b59bfbfeaded070401
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2022-07-07 17:04:47 +0300

    Update tests

--- tests/gravity/utils.ts
+++ tests/gravity/utils.ts
@@ -7,7 +7,7 @@ import { handleNewGravatar } from "../../src/gravity"
 
 export function handleNewGravatars(events: NewGravatar[]): void {
   events.forEach(event => {
-      handleNewGravatar(event)
+    handleNewGravatar(event)
   })
 }
 
@@ -18,8 +18,9 @@ export function saveGravatarFromContract(gravatarId: string): void {
   let contractGravatar = contract.getGravatar(contractAddress)
 
   let gravatar = new Gravatar(gravatarId)
-  gravatar.setString("value0", contractGravatar.value0)
-  gravatar.setString("value1", contractGravatar.value1)
+  gravatar.displayName = contractGravatar.value0
+  gravatar.imageUrl = contractGravatar.value1
+  gravatar.owner = contractAddress
   gravatar.save()
 }
 
@@ -27,28 +28,28 @@ export function trySaveGravatarFromContract(gravatarId: string): void {
   let contractGravatar = contract.try_getGravatar(contractAddress)
 
   if (!contractGravatar.reverted) {
-      let contractGravatarValue = contractGravatar.value
-      let gravatar = new Gravatar(gravatarId)
-      gravatar.setString("value0", contractGravatarValue.value0)
-      gravatar.setString("value1", contractGravatarValue.value1)
-      gravatar.save()
+    let gravatar = new Gravatar(gravatarId)
+    gravatar.displayName = "Gravatar 48 new"
+    gravatar.imageUrl = "https://example.com/image48_new.png"
+    gravatar.owner = contractAddress
+    gravatar.save()
   }
 }
 
 export function createNewGravatarEvent(id: i32, ownerAddress: string, displayName: string, imageUrl: string): NewGravatar {
-    let newGravatarEvent = changetype<NewGravatar>(newMockEvent())
-    newGravatarEvent.parameters = new Array()
-    let idParam = new ethereum.EventParam("id", ethereum.Value.fromI32(id))
-    let addressParam = new ethereum.EventParam("ownderAddress", ethereum.Value.fromAddress(Address.fromString(ownerAddress)))
-    let displayNameParam = new ethereum.EventParam("displayName", ethereum.Value.fromString(displayName))
-    let imageUrlParam = new ethereum.EventParam("imageUrl", ethereum.Value.fromString(imageUrl))
+  let newGravatarEvent = changetype<NewGravatar>(newMockEvent())
+  newGravatarEvent.parameters = new Array()
+  let idParam = new ethereum.EventParam("id", ethereum.Value.fromI32(id))
+  let addressParam = new ethereum.EventParam("ownderAddress", ethereum.Value.fromAddress(Address.fromString(ownerAddress)))
+  let displayNameParam = new ethereum.EventParam("displayName", ethereum.Value.fromString(displayName))
+  let imageUrlParam = new ethereum.EventParam("imageUrl", ethereum.Value.fromString(imageUrl))
 
-    newGravatarEvent.parameters.push(idParam)
-    newGravatarEvent.parameters.push(addressParam)
-    newGravatarEvent.parameters.push(displayNameParam)
-    newGravatarEvent.parameters.push(imageUrlParam)
+  newGravatarEvent.parameters.push(idParam)
+  newGravatarEvent.parameters.push(addressParam)
+  newGravatarEvent.parameters.push(displayNameParam)
+  newGravatarEvent.parameters.push(imageUrlParam)
 
-    return newGravatarEvent
+  return newGravatarEvent
 }
 
 export function processGravatar(value: JSONValue, userData: Value): void {
@@ -56,19 +57,23 @@ export function processGravatar(value: JSONValue, userData: Value): void {
   // with JSON values
   let obj = value.toObject()
   let id = obj.get('id')
+  let owner = obj.get('owner')
+  let imageUrl = obj.get('imageUrl')
 
-  if (!id) {
+  if (!id || !imageUrl || !owner) {
     return
   }
 
   // Callbacks can also created entities
   let gravatar = new Gravatar(id.toString())
   gravatar.displayName = userData.toString() + id.toString()
+  gravatar.imageUrl = imageUrl.toString()
+  gravatar.owner = Address.fromString(owner.toString())
   gravatar.save()
 }
 
 export function gravatarFromIpfs(): void {
-  let rawData = ipfs.cat("ipfsCatfileHash")
+  let rawData = ipfs.cat("ipfsCatFileHash")
 
   if (!rawData) {
     return
@@ -77,13 +82,16 @@ export function gravatarFromIpfs(): void {
   let jsonData = json.fromBytes(rawData as Bytes).toObject()
 
   let id = jsonData.get('id')
-  let url = jsonData.get("imageUrl")
+  let imageUrl = jsonData.get("imageUrl")
+  let owner = jsonData.get("owner")
 
-  if (!id || !url) {
+  if (!id || !imageUrl || !owner) {
     return
   }
 
   let gravatar = new Gravatar(id.toString())
-  gravatar.imageUrl = url.toString()
+  gravatar.imageUrl = imageUrl.toString()
+  gravatar.owner = Address.fromString(owner.toString())
+  gravatar.displayName = "Gravatar " + id.toString()
   gravatar.save()
 }
